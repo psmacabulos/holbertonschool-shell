3b0017f2f4da03f2a1e3abfd83adbe617b516e53
psmacabulos <patrick.macabulos@gmail.com>
2025-10-19 02:01:49 +0000

    Updated startup to  match color preference

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 git config --global user.name "psmacabulos"
 git config --global user.email "patrick.macabulos@gmail.com"
 git config --global credential.helper 'cache --timeout=21600'
-export PS1 = '\w \$ '
+export PS1='\[\e[1;35m\]\w\[\e[0m\] \$ '
