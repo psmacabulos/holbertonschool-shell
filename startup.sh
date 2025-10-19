78f02cc8ed4c0277d9e9834e4cdaeeda769a6cb3
psmacabulos <patrick.macabulos@gmail.com>
2025-10-19 12:44:37 +1100

    Added some statup script

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global user.name "psmacabulos"
+git config --global user.email "patrick.macabulos@gmail.com"
+git config --global credential.helper 'cache --timeout=21600'
+export PS1 = '\w \$ '
